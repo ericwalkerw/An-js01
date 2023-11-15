7b0835b336a06eafda00c33b2a83fc7b58148270
ericwalkerw <ericwalker@gmail.com>
2023-11-15 12:18:39 +0700

    add lab4

--- JS/lab2/ex2.js
+++ JS/lab2/ex2.js
@@ -1,5 +1,3 @@
 const rand = (min, max) =>  Math.floor(Math.random() * (max - min)) + min;
-
 let test = rand(1, 11);
-
 console.log(test);
--- JS/lab4/ex1.js
+++ JS/lab4/ex1.js
@@ -1,9 +1,24 @@
-const test = 247;
-
-function convertRoman(num){
-
-
-    return roman;
-}
-
-console.log(convertRoman(test));
+function convertToRoman(num) {
+    if(num < 0 || num > 1000) return;
+    const thousands = ["", "M"];
+    const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  
+    const thousandDigit = Math.floor(num / 1000);
+    const hundredDigit = Math.floor((num % 1000) / 100);
+    const tenDigit = Math.floor((num % 100) / 10);
+    const oneDigit = num % 10;
+  
+    return (
+      thousands[thousandDigit] +
+      hundreds[hundredDigit] +
+      tens[tenDigit] +
+      ones[oneDigit]
+    );
+  }
+  
+  // Ví dụ sử dụng
+  const number = 246;
+  const romanNumeral = convertToRoman(number);
+  console.log(`Số ${number} chuyển thành số La Mã là: ${romanNumeral}`);
